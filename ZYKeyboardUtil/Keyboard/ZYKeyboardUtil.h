0434e0ecec1b2492295fcbb19d3e5de132034d69
liuzhiyi1992 <55137840@qq.com>
2016-01-07 15:59:58 +0800

    commit master

---
+++ ZYKeyboardUtil/Keyboard/ZYKeyboardUtil.h
@@ -0,0 +1,65 @@
+//
+//  ZYKeyboardUtil.h
+//  ZYKeyboardUtil
+//
+//  Created by lzy on 15/12/26.
+//  Copyright © 2015年 lzy . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+typedef enum {
+    KeyboardActionDefault,
+    KeyboardActionShow,
+    KeyboardActionHide
+}KeyboardAction;
+
+//typedef struct {
+//    CGFloat animationDuration;
+//    CGRect frameBegin;
+//    CGRect frameEnd;
+//    CGFloat heightIncrement;
+//    KeyboardAction action;
+//    BOOL isSameAction;
+//    //    animation的Curve(option)(考虑)
+//}KeyboardInfo;
+
+
+#pragma mark - KeyboardInfo(model)
+@interface KeyboardInfo : NSObject
+
+@property (assign, nonatomic) CGFloat animationDuration;
+@property (assign, nonatomic) CGRect frameBegin;
+@property (assign, nonatomic) CGRect frameEnd;
+@property (assign, nonatomic) CGFloat heightIncrement;
+@property (assign, nonatomic) KeyboardAction action;
+@property (assign, nonatomic) BOOL isSameAction;
+
+
+- (void)fillKeyboardInfoWithDuration:(CGFloat)duration frameBegin:(CGRect)frameBegin frameEnd:(CGRect)frameEnd heightIncrement:(CGFloat)heightIncrement action:(KeyboardAction)action isSameAction:(BOOL)isSameAction;
+
+@end
+
+
+
+
+
+#pragma mark - ZYKeyboardUtil
+@interface ZYKeyboardUtil : NSObject
+
+//Block
+typedef void (^animateWhenKeyboardAppearBlock)(int appearPostIndex, CGRect keyboardRect, CGFloat keyboardHeight, CGFloat keyboardHeightIncrement);
+typedef void (^animateWhenKeyboardDisappearBlock)(CGFloat keyboardHeight);
+typedef void (^printKeyboardInfoBlock)(ZYKeyboardUtil *keyboardUtil, KeyboardInfo *keyboardInfo);
+
+
+- (void)setAnimateWhenKeyboardAppearBlock:(animateWhenKeyboardAppearBlock)animateWhenKeyboardAppearBlock;
+
+- (void)setAnimateWhenKeyboardDisappearBlock:(animateWhenKeyboardDisappearBlock)animateWhenKeyboardDisappearBlock;
+
+- (void)setPrintKeyboardInfoBlock:(printKeyboardInfoBlock)printKeyboardInfoBlock;
+
+@end
+
+
--- ZYKeyboardUtil/ViewController.m
+++ ZYKeyboardUtil/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "ZYKeyboardUtil.h"
+
+#define MARGIN_KEYBOARD 20
 
 @interface ViewController ()
+
+@property (strong, nonatomic) ZYKeyboardUtil *keyboardUtil;
+@property (weak, nonatomic) IBOutlet UITextField *mainTextField;
 
 @end
 
@@ -16,12 +22,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _mainTextField.delegate = self;
+    
+    [self configKeyBoardRespond];
+}
+
+- (void)configKeyBoardRespond {
+     self.keyboardUtil = [[ZYKeyboardUtil alloc] init];
+    
+    __unsafe_unretained ViewController *weakSelf = self;
+    
+    [_keyboardUtil setAnimateWhenKeyboardAppearBlock:^(int appearPostIndex, CGRect keyboardRect, CGFloat keyboardHeight, CGFloat keyboardHeightIncrement) {
+        
+        NSLog(@"\n\n键盘弹出来第 %d 次了~  高度比上一次增加了%0.f  当前高度是:%0.f"  , appearPostIndex, keyboardHeightIncrement, keyboardHeight);
+        
+        //处理
+        UIWindow *window = [UIApplication sharedApplication].keyWindow;
+        CGRect convertRect = [weakSelf.mainTextField.superview convertRect:weakSelf.mainTextField.frame toView:window];
+        
+        if (CGRectGetMinY(keyboardRect) - MARGIN_KEYBOARD < CGRectGetMaxY(convertRect)) {
+            CGFloat signedDiff = CGRectGetMinY(keyboardRect) - CGRectGetMaxY(convertRect) - MARGIN_KEYBOARD;
+            //uodateOriginY
+            CGFloat newOriginY = CGRectGetMinY(weakSelf.view.frame) + signedDiff;
+            weakSelf.view.frame = CGRectMake(weakSelf.view.frame.origin.x, newOriginY, weakSelf.view.frame.size.width, weakSelf.view.frame.size.height);
+        }
+    }];
+    
+    
+    [self.keyboardUtil setAnimateWhenKeyboardDisappearBlock:^(CGFloat keyboardHeight) {
+        NSLog(@"\n\n键盘在收起来~  上次高度为:+%f", keyboardHeight);
+        
+        //uodateOriginY
+        CGFloat newOriginY = 0;
+        weakSelf.view.frame = CGRectMake(weakSelf.view.frame.origin.x, newOriginY, weakSelf.view.frame.size.width, weakSelf.view.frame.size.height);
+    }];
+    
+    
+    [self.keyboardUtil setPrintKeyboardInfoBlock:^(ZYKeyboardUtil *keyboardUtil, KeyboardInfo *keyboardInfo) {
+        
+        NSLog(@"\n\n拿到键盘信息 和 ZYKeyboardUtil对象");
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark delegate
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.mainTextField resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+   [self.mainTextField resignFirstResponder];
+    return YES;
+}
+
 
 @end
